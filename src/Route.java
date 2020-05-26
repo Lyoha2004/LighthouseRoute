44141c8a64f8c3ab1d2f44206427e9d095f93f5c
SappyJoy <stponomarevw@gmail.com>
2020-05-26 14:46:19 +0500

    Wrong heuristic

--- src/Route.java
+++ src/Route.java
@@ -53,7 +53,7 @@ public class Route {
                 double newCost = costSoFar.get(current) + moveCost(current, next);
                 if (!costSoFar.containsKey(next) || newCost < costSoFar.get(next)) {
                     costSoFar.put(next, newCost);
-                    double priority = newCost + heuristic(next, b);
+                    double priority = newCost;
                     frontier.add(new CoordinateWithCost(priority, next));
                     cameFrom.put(next, current);
                 }
